62a1bbd16eef63e943ebab9725ccf555d2213235
ne1son <ne1s0n@users.noreply.github.com>
2016-05-04 14:23:23 +0200

    update to compiled joint tex source

--- redo_pdf.sh
+++ redo_pdf.sh
@@ -22,3 +22,16 @@ pdflatex $FILE
 bibtex $FILE
 pdflatex $FILE
 pdflatex $FILE
+
+#DOING MAIN WITH ALL INPUTS IN ONE FILE
+FILE=Nazzicari_Imputation
+
+#cleaning all LaTeX clutter
+rm -f $FILE.aux $FILE.dvi $FILE.log $FILE.idx $FILE.ind $FILE.toc $FILE.ilg $FILE.out $FILE.bbl $FILE.blg $FILE.nav $FILE.snm $FILE.ps $FILE.pdf
+
+#doing the usual latex routine
+pdflatex $FILE
+bibtex $FILE
+pdflatex $FILE
+pdflatex $FILE
+
