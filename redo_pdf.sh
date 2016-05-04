5c5be95f7571e949d887f8fe1ca3652378b3b873
ne1son <ne1s0n@users.noreply.github.com>
2016-05-04 14:34:46 +0200

    updates to have all supplementary in a single joint file

--- redo_pdf.sh
+++ redo_pdf.sh
@@ -35,3 +35,15 @@ bibtex $FILE
 pdflatex $FILE
 pdflatex $FILE
 
+#DOING SUPPL WITH ALL INPUTS IN ONE FILE
+FILE=Nazzicari_Imputation_suppl
+
+#cleaning all LaTeX clutter
+rm -f $FILE.aux $FILE.dvi $FILE.log $FILE.idx $FILE.ind $FILE.toc $FILE.ilg $FILE.out $FILE.bbl $FILE.blg $FILE.nav $FILE.snm $FILE.ps $FILE.pdf
+
+#doing the usual latex routine
+pdflatex $FILE
+bibtex $FILE
+pdflatex $FILE
+pdflatex $FILE
+
